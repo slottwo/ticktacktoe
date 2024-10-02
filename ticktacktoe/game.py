2b888c82db0bb221b6db0482a0b3b639dc6b5d41
slottwo <41028091+slottwo@users.noreply.github.com>
2024-10-02 14:17:34 -0300

    fix tuple indexes

--- ticktacktoe/game.py
+++ ticktacktoe/game.py
@@ -5,21 +5,21 @@ class Game:
     def __len__(self) -> int:
         return 9 - self.state.count(' ')
 
-    def __getitem__(self, *index: int):
+    def __getitem__(self, index: int):
         if len(index) == 1:
             return self.state[index[0]]
         if len(index) == 2:
             return self.state[index[0] + index[1] * 3]
-        raise IndexError
+        raise IndexError(f"{len(index)}-dimensional indexation not supported")
 
     def value(self):
         if len(self) < 5:  # optimization: turn 5: round 3
-            return 0
+            return 0  # too early
         _ = self.state
         _ = _[:3], _[3:6], _[6:], _[::3], _[1::3], _[2::3], _[::4], _[2:-2:2]
         if b'xxx' in _:
-            return 1
-        return -(b'ooo' in _)
+            return 1  # win 1
+        return -(b'ooo' in _)  # lose -1 // draw 0
 
     @property
     def state(self):
